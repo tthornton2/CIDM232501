0fcc0a86beadc3c37961c4b14362e3938c3935e9
tthornton2 <143360268+tthornton2@users.noreply.github.com>
2023-08-27 18:03:56 -0500

    Homework 1 Completed Code

---
+++ HW1_Completed.cs
@@ -0,0 +1,13 @@
+﻿namespace HW1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal x = 2.5m;
+        decimal y = 3.3m;
+        decimal result = 4*x*x + 3*y;
+
+        Console.WriteLine(result);
+    }
+}
