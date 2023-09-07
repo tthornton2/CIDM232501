a0c8174d3c66f8a510c336a1b5cdcbfc51d21601
tthornton2 <143360268+tthornton2@users.noreply.github.com>
2023-09-06 21:22:21 -0500

    Add files via upload

---
+++ HW2_1.cs
@@ -0,0 +1,33 @@
+﻿namespace HW2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your letter grade.");
+        string letter = Console.ReadLine();
+ 
+
+        if (letter == "A"){
+            Console.WriteLine("4 GPA Points");
+        }
+        else if (letter == "B"){ 
+            Console.WriteLine("3 GPA Points");
+        }
+        else if (letter == "C"){ 
+            Console.WriteLine("2 GPA Points");
+        }
+        else if (letter == "D"){ 
+            Console.WriteLine("1 GPA Points");
+        }
+        else if (letter == "F"){
+            Console.WriteLine("0 GPA Points");
+        }
+        else
+        {
+            Console.WriteLine("Try again!");
+        }
+        
+
+    }
+}
